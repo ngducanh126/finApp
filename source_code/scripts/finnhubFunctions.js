d3157a3db040b50865aa355e241a55ef91a8cc98
anh <ngducanh126@gmail.com>
2024-01-02 18:29:31 -0500

    Initial commit: getStockQuote for real-time price snapshot

---
+++ source_code/scripts/finnhubFunctions.js
@@ -0,0 +1,6 @@
+async function getStockQuote(symbol, apiKey) {
+    const url = `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${apiKey}`;
+    const res = await fetch(url);
+    return await res.json();
+}
+
