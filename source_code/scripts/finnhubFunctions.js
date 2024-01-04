2721b73f801fa1f3ef0c550f87feb350b899d0f5
anh <ngducanh126@gmail.com>
2024-01-03 19:21:45 -0500

    Added getCompanyProfile to retrieve company details

--- source_code/scripts/finnhubFunctions.js
+++ source_code/scripts/finnhubFunctions.js
@@ -4,3 +4,9 @@ async function getStockQuote(symbol, apiKey) {
     return await res.json();
 }
 
+async function getCompanyProfile(symbol, apiKey) {
+    const url = `https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${apiKey}`;
+    const res = await fetch(url);
+    return await res.json();
+}
+
