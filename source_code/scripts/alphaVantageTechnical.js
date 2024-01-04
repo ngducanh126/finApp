778907151ec8c2499c585b3da842bb2b31ce19db
anh <ngducanh126@gmail.com>
2024-01-03 19:31:08 -0500

    integrated getLatestClose into alphaVantageTechnical.js

--- source_code/scripts/alphaVantageTechnical.js
+++ source_code/scripts/alphaVantageTechnical.js
@@ -5,3 +5,9 @@ async function fetchDailyTimeSeries(symbol, apiKey) {
     return data["Time Series (Daily)"];
 }
 
+async function getLatestClose(symbol, apiKey) {
+    const series = await fetchDailyTimeSeries(symbol, apiKey);
+    const dates = Object.keys(series).sort().reverse();
+    return parseFloat(series[dates[0]]["4. close"]);
+}
+
