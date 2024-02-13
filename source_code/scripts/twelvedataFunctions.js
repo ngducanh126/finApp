0afeaa6e1c5a15b26c5984f5ac529a9e41e7ca04
anh <ngducanh126@gmail.com>
2024-02-12 22:02:08 -0500

    Added getTimeSeries for flexible asset data retrieval

--- source_code/scripts/twelvedataFunctions.js
+++ source_code/scripts/twelvedataFunctions.js
@@ -389,3 +389,9 @@ async function getRollingBeta(symbol, benchmark, interval, apiKey) {
     return cov / var2;
 }
 
+async function getTimeSeries(symbol, interval, apiKey) {
+    const url = `https://api.twelvedata.com/time_series?symbol=${symbol}&interval=${interval}&apikey=${apiKey}`;
+    const res = await fetch(url);
+    return await res.json();
+}
+
