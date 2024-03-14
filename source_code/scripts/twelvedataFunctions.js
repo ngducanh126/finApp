7cf83214a564247cb806baf047d712aa9efa40c9
anh <ngducanh126@gmail.com>
2024-03-13 20:52:11 -0400

    getETFPerformance now compares ETF returns

--- source_code/scripts/twelvedataFunctions.js
+++ source_code/scripts/twelvedataFunctions.js
@@ -247,3 +247,9 @@ async function getSectorLeaders(apiKey) {
     return (await res.json()).data;
 }
 
+async function getETFPerformance(apiKey) {
+    const url = `https://api.twelvedata.com/etf?apikey=${apiKey}`;
+    const res = await fetch(url);
+    return (await res.json()).data;
+}
+
