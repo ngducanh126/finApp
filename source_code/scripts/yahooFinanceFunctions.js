bd0026eccb7b7595793cff400b6c04194cc60915
anh <ngducanh126@gmail.com>
2024-05-12 23:13:38 -0400

    Now supporting getTrendingTickers for market buzz

--- source_code/scripts/yahooFinanceFunctions.js
+++ source_code/scripts/yahooFinanceFunctions.js
@@ -10,3 +10,9 @@ async function getHistoricalPrices(symbol, period1, period2, interval, apiKey) {
     return await res.json();
 }
 
+async function getTrendingTickers(apiKey) {
+    const url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-trending-tickers?region=US`;
+    const res = await fetch(url, { headers: { 'x-rapidapi-key': apiKey } });
+    return await res.json();
+}
+
